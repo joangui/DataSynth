46c149de460448138838e7b55bfa4e3a3be998cc
Arnau  Prat <aprat@ac.upc.edu>
2017-03-16 17:43:12 +0100

    Added command line options to UnbalancedPartitionsTest

--- src/main/java/org/dama/datasynth/matching/test/UnbalancedPartitionsTest.java
+++ src/main/java/org/dama/datasynth/matching/test/UnbalancedPartitionsTest.java
@@ -5,6 +5,8 @@
  */
 package org.dama.datasynth.matching.test;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import org.dama.datasynth.matching.Table;
 import org.dama.datasynth.matching.Tuple;
 import org.dama.datasynth.matching.graphs.BFSTraversal;
@@ -26,32 +28,53 @@ import java.util.*;
 public class UnbalancedPartitionsTest {
 
 	static Random r = new Random(1234567890L);
-	static int NUM_ATTRIBUTES = 4;
-	static double k = 0.4;
+
+	private static class Options {
+
+		@Parameter(names = {"-o","--output"}, description = "Path to the output file", required = true)
+		public String outputFile;
+
+		@Parameter(names = {"-i","--input"}, description = "Path to the input file", required = true)
+		public String inputFile;
+
+
+		@Parameter(names = {"-n","--nattributes"}, description = "Number of attributes to generate")
+		public int numAttributes = 4;
+
+		@Parameter(names = {"-k","--kparam"}, description = "Value of the k parameter of the proportions assignment distribution")
+		public double k = 0.4;
+
+	}
+
 
 	static public void main(String[] argv) throws Exception {
+
+	    Options options = new Options();
+		JCommander commander = new JCommander(options,argv);
+
+
 		System.out.println("Unbalanced Partition Test");
 		Table<Long, Integer> attributes = new Table<>();
 
 		Table<Long, Long> edges = new Table<>();
 		// Load edges table
 		try {
-			FileInputStream	fileInputStream = new FileInputStream(argv[0]);
+			FileInputStream	fileInputStream = new FileInputStream(options.inputFile);
 			edges.load(fileInputStream, " ", (String s) -> Long.parseLong(s), (String s) -> Long.parseLong(s));
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 
-		double proportions[] = new double[NUM_ATTRIBUTES];
+		double proportions[] = new double[options.numAttributes];
 		double sum = 0.0;
-		for(int i = 1; i <= NUM_ATTRIBUTES; ++i) {
-			double prob = Math.max(Math.pow(1-k,i-1)*k,1/(double)NUM_ATTRIBUTES);
+		for(int i = 1; i <= options.numAttributes; ++i) {
+			double prob = Math.max(Math.pow(1-options.k,i-1)*options.k,1/(double)options.numAttributes);
 			sum+=prob;
 			proportions[i-1] = prob;
 		}
 
-		for(int i = 0; i < NUM_ATTRIBUTES; ++i) {
+		for(int i = 0; i < options.numAttributes; ++i) {
 			proportions[i] = proportions[i] / sum;
 		}
 
@@ -62,7 +85,7 @@ public class UnbalancedPartitionsTest {
 			Integer partitionId = entry.getKey();
 			Set<Long> nodes = entry.getValue();
 
-			Integer attribute = partitionId % NUM_ATTRIBUTES;
+			Integer attribute = partitionId % options.numAttributes;
 
 			for (long nodeId : nodes) {
 				attributes.add(new Tuple<>(nodeId, attribute));
@@ -93,8 +116,7 @@ public class UnbalancedPartitionsTest {
 		DistributionStatistics.DMaxStatistics dMaxStatistics = ds.dMaxTest();
 		System.out.println("\nDmax: " + dMaxStatistics.dMaxValue);
 
-		String fileDmaxPath = "./plots/100K-"+NUM_ATTRIBUTES+".txt";
-		printFile(fileDmaxPath,dMaxStatistics.accumulativeExpectedProbValues,dMaxStatistics.accumulativeObservedProbValues);
+		printFile(options.outputFile,dMaxStatistics.accumulativeExpectedProbValues,dMaxStatistics.accumulativeObservedProbValues);
 		}catch(Exception e)
 		{
 			e.printStackTrace();
