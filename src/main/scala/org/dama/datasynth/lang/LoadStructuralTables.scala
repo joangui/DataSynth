662306fef7c0fbfb57c9e49fc245fdbdb6b0b430
joangui <joan@ac.upc.edu>
2017-05-15 16:27:39 -0500

    LoadStructuralTables.scala code issued fixed. (#13)
    * Code issues fixed

--- src/main/scala/org/dama/datasynth/lang/LoadStructuralTables.scala
+++ src/main/scala/org/dama/datasynth/lang/LoadStructuralTables.scala
@@ -24,65 +24,57 @@ object LoadStructuralTables{
 
     //For each edge type
     edgeTypes.flatMap(edgeType => {
-      var nodes:Seq[Table]=Seq()
+     // var nodes:Seq[Table]=Seq()
 
       //If the edge has a correlation we create a temporal edgeTable which names adds the suffix .structure
       val edgeName:String = if(edgeType.correlation.isDefined) s"${edgeType.name}.structure" else edgeType.name
       val source:String = edgeType.source
       val target:String = edgeType.target
-
       val structure:Structure = edgeType.structure
 
-      val sourceNode:NodeType= nodeTypes.find(_.name==source).get
-      val targetNode:NodeType= nodeTypes.find(_.name==target).get
+      val sourceNode:NodeType= nodeTypes.find(_.name==source) match {case None => throw new RuntimeException(s"There is no node type called $source.") case Some(nodeT)=> nodeT}
+      val targetNode:NodeType= nodeTypes.find(_.name==target) match {case None => throw new RuntimeException(s"There is no node type called $target.") case Some(nodeT)=> nodeT}
 
       val structureGenerator:StructureGenerator=createStructureGenerator(structure)
 
       val size:StaticValue[Long] = StaticValue[Long](
-      if(source==target)
-        sourceNode.instances
-      else
-        sourceNode.instances+targetNode.instances)
+      if(source==target) sourceNode.instances
+      else sourceNode.instances+targetNode.instances)
 
       //We create the corresponding edgeTable
       val edgeTable = EdgeTable(edgeName,structureGenerator,size)
-      nodes=nodes:+edgeTable
+      val edgeTableNodes : Seq[Table] = Seq(edgeTable)
 
       //If the edge is correlated we create a Match table
-      if(edgeType.correlation.isDefined)
-        {
-          val filename:String = edgeType.correlation.get.filename
-          val sourceProperty:String = edgeType.correlation.get.source
-          val targetProperty:String = edgeType.correlation.get.target
+      edgeType.correlation match{
+        case None =>edgeTableNodes
+        case Some (correlation) => {
+        val filename: String = correlation.filename
+        val sourceProperty: String = correlation.source
+        val targetProperty: String = correlation.target
+        val sourcePropertyTableName = s"$source.$sourceProperty"
+        val targetPropertyTableName = s"$target.$targetProperty"
 
-          val sourcePropertyTableName = s"$source.$sourceProperty"
-          val targetPropertyTableName = s"$target.$targetProperty"
+        val sourcePropertyTable: PropertyTable[_] = propertyTables.find(_.name == sourcePropertyTableName) match {
+          case Some(propertyTable) => propertyTable
+          case None => throw new RuntimeException(s"There is no PropertyTable for ${sourcePropertyTableName}. Should never happen: check schema validation.") }
+        val targetPropertyTable: PropertyTable[_] = propertyTables.find(_.name == targetPropertyTableName) match {
+          case Some(propertyTable) => propertyTable
+          case None => throw new RuntimeException(s"There is no PropertyTable for ${targetPropertyTableName}. Should never happen: check schema validation.") }
 
-          val sourcePropertyTable:PropertyTable[_]= propertyTables.find(_.name==sourcePropertyTableName).get
-          val targetPropertyTable:PropertyTable[_]= propertyTables.find(_.name==targetPropertyTableName).get
-
-          val matchTable : Table = Match(edgeType.name,sourcePropertyTable,targetPropertyTable,edgeTable,filename)
-
-          nodes=nodes:+matchTable
-
-        }
-
-      nodes
-    })
+        val matchTable: Table = Match(edgeType.name, sourcePropertyTable, targetPropertyTable, edgeTable, filename)
+        val edgeAndMatchTableNodes :Seq[Table]=Seq(edgeTable,matchTable)
+          edgeAndMatchTableNodes
+      } } })
   }
 
 
   def createStructureGenerator(structure: Structure): StructureGenerator = {
-
-    val values :  Seq[Value[_]] =
-      if( structure.initParameters.isDefined)
-      {
-        ReadExecutionPlan.readInitParameters(structure.initParameters.get)
-      }
-      else
-      {
-        Seq()
-      }
+    val values :  Seq[Value[_]] = structure.initParameters match {
+      case Some(initParameters) =>
+        ReadExecutionPlan.readInitParameters(initParameters)
+      case None => Seq()
+    }
     StructureGenerator(structure.name,values)
   }
 
