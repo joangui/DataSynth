bb142b829aca480a63a3b6961cec617bc6a45da4
joangui <joan@ac.upc.edu>
2017-05-15 13:24:25 -0500

    LoadPropertyTables.scala code errors fixes. (#12)
    * Fix 1 error regarding Option usages
    
    * Fix 1 error regarding Option usages
    
    * Code issues fixes
    
    * Code errors fixed
    
    * Code errors fixed
    
    * LoadPropertyTable code issues fixed.

--- src/main/scala/org/dama/datasynth/lang/LoadPropertyTables.scala
+++ src/main/scala/org/dama/datasynth/lang/LoadPropertyTables.scala
@@ -14,149 +14,133 @@ import scala.collection.mutable
 object LoadPropertyTables{
 
 
- /**
-   * propertyMap is a variable that is reseted for each node Type. It is used to control which property tables
-   * can be generated as it depends on their dependencies
-   */
- var propertyMap = new mutable.HashMap[String,PropertyTable[_]]()
-
- /**
-   *
-   * @param nodeTypes list of nodeTypes
-   * @return the PropertyTables that correspond to properties of the nodes in nodeTypes
-   */
- def getPropertyTableNodes(nodeTypes:Seq[NodeType]):Seq[PropertyTable[_]] =
- {
-
-   val propertyNodes:Seq[NodeType]=  nodeTypes.filter(_.properties.isDefined);
-
-  val propertyTables:Seq[PropertyTable[_]] = propertyNodes.flatMap(nodeType =>
-  {
-   //We reset the propertyMap for each NodeType
-   propertyMap = new mutable.HashMap[String, PropertyTable[_]]()
-
-
-   //First we create the PropertyTables which Generator do not depend on any other
-   val independentProperties:Seq[Property] = nodeType.properties.get.filter(property=>
-   {!property.generator.dependencies.isDefined||(property.generator.dependencies.get.isEmpty)})
-
-   val independentPropertyTables:Seq[PropertyTable[_]]=getPropertyTables(nodeType,independentProperties)
-
-
-   var dependentPropertiesCandidates:Seq[Property] = nodeType.properties.get.filter(property=>
-   {property.generator.dependencies.isDefined && property.generator.dependencies.get.nonEmpty})
-
-
-   var dependentPropertyTables:Seq[PropertyTable[_]]=Seq()
-
-   //We create the PropertyTables as soon as its dependencies have been created
-   while (dependentPropertiesCandidates.nonEmpty)
-   {
-    val dependentProperties = dependentPropertiesCandidates.filter(dependentProperty=>
-     isSubset(dependentProperty.generator.dependencies.get,propertyMap.keySet))
-
-    dependentPropertyTables=dependentPropertyTables ++ getPropertyTables(nodeType,dependentProperties)
-
-    dependentPropertiesCandidates=dependentPropertiesCandidates diff dependentProperties
-   }
-   independentPropertyTables++dependentPropertyTables
-  })
-
-  propertyTables
- }
-
- /**
-   *
-   * @param subset set of strings
-   * @param set set of string
-   * @return subset is subset of set
-   */
- private def isSubset(subset:Seq[String],set:scala.collection.Set[String]):Boolean=
- {
-  var result = true
-  subset.foreach(element=>{
-   result&=set.contains(element)
-  })
-  result
- }
-
- /**
-   *
-   * @param nodeType given a nodeType.
-   * @param properties and the set of its properties which PropertyTable is ready to be created.
-   * @return the sequence of ExectionPlanNodes with the PropertyTables
-   */
- private def getPropertyTables(nodeType:NodeType,properties:Seq[Property] ):Seq[PropertyTable[_]]=
-
- {
-  properties.map(property => {
-
-   //Disambiguate the property type to call the correct function
-   property.dataType.toLowerCase match {
-    case "string" =>   getPropertyTable[String](nodeType.name, nodeType.instances, property)
-    case "int" =>   getPropertyTable[Int](nodeType.name, nodeType.instances, property)
-    case "long" =>   getPropertyTable[Long](nodeType.name, nodeType.instances, property)
-    case "double" =>  getPropertyTable[Double](nodeType.name, nodeType.instances, property)
-    case "float" =>   getPropertyTable[Float](nodeType.name, nodeType.instances, property)
-   }
-
-  }
-  )
- }
-
- //Creates the PropertyTable
- /**
-   *
-   * @param nodeName to which it belongs the propertyTable
-   * @param instances number of instances
-   * @param property property of type T
-   * @return
-   */
- def getPropertyTable[T : TypeTag](nodeName:String, instances:Long, property : Property) : PropertyTable[T]=
- {
-  val propertyName = property.name;
-  val generator = property.generator;
-
-  val propertyTable = PropertyTable[T](nodeName,propertyName,getPropertyGenerator[T](generator),StaticValue[Long](instances))
-  propertyMap.put(propertyName,propertyTable)
-  propertyTable
- }
-
-
-
+  /**
+    * propertyMap is a variable that is reseted for each node Type. It is used to control which property tables
+    * can be generated as it depends on their dependencies
+    */
+  val propertyMap = new mutable.HashMap[String,PropertyTable[_]]()
 
   /**
-   *
-   * @param generator defines the generator's characteristics
-   * @return the PropertyGenerator
-   */
- def getPropertyGenerator[T](generator:Generator):PropertyGenerator[T]=
- {
-  val generatorName = generator.name
+    *
+    * @param subset set of strings
+    * @param set set of string
+    * @return subset is subset of set
+    */
+  private def isSubset(subset:Seq[String],set:scala.collection.Set[String]):Boolean=
+  {
+    subset.foldLeft(true) ({case (result,element)=> result&set.contains(element) })
+  }
 
-  val propertyTables : Seq[PropertyTable[_]] =
-   if(generator.dependencies.isDefined) {
-     generator.dependencies.get.map(
-       dependency=>
-       {
-         propertyMap.get(dependency).get
-       }
-     )
-   }
-   else
+  /**
+    *
+    * @param nodeTypes list of nodeTypes
+    * @return the PropertyTables that correspond to properties of the nodes in nodeTypes
+    */
+  def getPropertyTableNodes(nodeTypes:Seq[NodeType]):Seq[PropertyTable[_]] =
+  {
+    val propertyTables:Seq[PropertyTable[_]] = nodeTypes.flatMap(nodeType =>
+    {
+      //We reset the propertyMap for each NodeType
+      propertyMap.clear()
+
+      //First we create the PropertyTables which Generator do not depend on any other
+      val independentProperties:Seq[Property] = nodeType.properties match {
+        case None => Seq()
+        case Some(properties) =>properties.filter (property =>
+          property.generator.dependencies match
+          { case None => true
+            case Some(dependency) => dependency.isEmpty }
+        )
+      }
+      val independentPropertyTables:Seq[PropertyTable[_]]=getPropertyTables(nodeType,independentProperties)
+      val dependentPropertiesCandidates:Seq[Property] = nodeType.properties match
+      {
+        case None => Seq()
+        case Some(properties) => properties.filter(property=>
+          property.generator.dependencies match {
+            case None => false
+            case Some(dependency) => dependency.nonEmpty })
+      }
+
+
+      //We create the PropertyTables as soon as its dependencies have been created
+      val dependentPropertyTables:Seq[PropertyTable[_]] = calculateDependentPropertyTables(nodeType,dependentPropertiesCandidates)
+      independentPropertyTables++dependentPropertyTables
+    })
+    propertyTables
+  }
+
+  def calculateDependentPropertyTables(nodeType: NodeType, dependentPropertiesCandidates: Seq[Property] ): Seq[PropertyTable[_]] = {
+    if (dependentPropertiesCandidates.isEmpty)
      Seq()
+    else {
+      val dependentProperties = dependentPropertiesCandidates.filter(dependentProperty=>
+        isSubset(dependentProperty.generator.dependencies match {case None => Seq() case Some(dependencies) =>dependencies},propertyMap.keySet))
+      val dependentPropertyTables:Seq[PropertyTable[_]] = getPropertyTables(nodeType,dependentProperties)
+      dependentPropertyTables ++ calculateDependentPropertyTables(nodeType, (dependentPropertiesCandidates diff dependentProperties))
+    }
+   // val dependentPropertyTables:Seq[PropertyTable[_]] =dependentPropertyTables ++ getPropertyTables(nodeType,dependentProperties)
+    //dependentPropertiesCandidates=dependentPropertiesCandidates diff dependentProperties
+  }
 
+  /**
+    *
+    * @param nodeType given a nodeType.
+    * @param properties and the set of its properties which PropertyTable is ready to be created.
+    * @return the sequence of ExectionPlanNodes with the PropertyTables
+    */
+  private def getPropertyTables(nodeType:NodeType,properties:Seq[Property] ):Seq[PropertyTable[_]]=
+  {
+    properties.map(property => {
 
+      //Disambiguate the property type to call the correct function
+      property.dataType.toLowerCase match {
+        case "string" =>   getPropertyTable[String](nodeType.name, nodeType.instances, property)
+        case "int" =>   getPropertyTable[Int](nodeType.name, nodeType.instances, property)
+        case "long" =>   getPropertyTable[Long](nodeType.name, nodeType.instances, property)
+        case "double" =>  getPropertyTable[Double](nodeType.name, nodeType.instances, property)
+        case "float" =>   getPropertyTable[Float](nodeType.name, nodeType.instances, property)
+      } } )
+  }
 
-   val values :  Seq[Value[_]] =
-     if( generator.initParameters.isDefined)
-     {
-       ReadExecutionPlan.readInitParameters(generator.initParameters.get)
-     }
-     else
-     {
-       Seq()
-     }
-   PropertyGenerator(generatorName,values,propertyTables)
- }
+  //Creates the PropertyTable
+  /**
+    *
+    * @param nodeName to which it belongs the propertyTable
+    * @param instances number of instances
+    * @param property property of type T
+    * @return
+    */
+  def getPropertyTable[T : TypeTag](nodeName:String, instances:Long, property : Property) : PropertyTable[T]=
+  {
+    val propertyName = property.name;
+    val generator = property.generator;
+
+    val propertyTable = PropertyTable[T](nodeName,propertyName,getPropertyGenerator[T](generator),StaticValue[Long](instances))
+    propertyMap.put(propertyName,propertyTable)
+    propertyTable
+  }
+
+  /**
+    *
+    * @param generator defines the generator's characteristics
+    * @return the PropertyGenerator
+    */
+  def getPropertyGenerator[T](generator:Generator):PropertyGenerator[T]=
+  {
+    val generatorName = generator.name
+
+    val propertyTables : Seq[PropertyTable[_]] =
+      generator.dependencies match
+      {
+        case None => Seq()
+        case Some(dependencies) => dependencies.map(dependency=>
+        {propertyMap.get(dependency) match{ case None=> throw  new RuntimeException("Weird Error, this should have never  " +
+          "happened: Probably the dependencies chain is ill-formed.")
+                                            case Some(dependencyTable) => dependencyTable }})
+      }
+
+    val values :  Seq[Value[_]] = ReadExecutionPlan.readInitParameters(generator.initParameters.getOrElse(Seq()))
+    PropertyGenerator(generatorName,values,propertyTables)
+  }
 }
